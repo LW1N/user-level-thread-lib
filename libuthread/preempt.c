66607d9dcaeaf6c83a2ae5af74d20ecb17dea305
Lucas Nguyen <lhvnguyen@ucdavis.edu>
2023-05-10 18:14:33 -0700

    Starting Phase 4

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,61 @@
  */
 #define HZ 100
 
+// Global timer variables
+struct sigaction sa;
+struct itimerval timer;
+bool preemption = false;
+
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
+	if(!preemption){
+		return;
+	}
+	sigprocmask(SIG_BLOCK, &blocksigs, NULL);
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+	if(!preemption){
+		return;
+	}
+	sigprocmask(SIG_UNBLOCK, &blocksigs, NULL);
 }
 
 void preempt_start(bool preempt)
 {
 	/* TODO Phase 4 */
+	if(!preempt){
+		return;
+	}
+	
+	preemption = true;
+
+	// Install signal handler for SIGVTALRM
+	memset(&sa, 0, sizeof(sa));
+
+	// Set signal handler for SIGVTALRM
+	sa.sa_handler = &timer_handler;
+	sa.sa_flags = SA_NOMASK | SA_ONSTACK;
+	// sigemptyset(&sa.sa_mask);
+	sigaction(SIGVTALRM, &sa, NULL);
+
+	// Set up timer to trigger SIGVTALRM every HZ per second
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = HZ;
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = HZ;
+
+	// Start a virtual timer
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);
+
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+	
 }
 
